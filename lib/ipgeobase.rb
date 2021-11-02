25c24bbf72c1d4ec22e6c52d940e7171b3ca1d65
Nikolay Gagarinov <feycot@gmail.com>
2021-11-02 20:38:08 +0500

    Update ipgeobase.rb

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,17 +1,14 @@
-# frozen_string_literal: true
-
 require "uri"
 require "open-uri"
 require "addressable"
 require_relative "ipgeobase/version"
 require "net/http"
-
 module Ipgeobase
   URL = "http://ip-api.com/xml"
   autoload "IpMetaData", "ipgeobase/ip_meta_data"
 
   def self.lookup(ip)
-    template = Addressable::Template.new("#{URL}{/ip}")
+      template = Addressable::Template.new("#{URL}{/ip}")
     uri = template.expand ip: ip
     response = Net::HTTP.get_response(uri)
     IpMetaData.parse(response.body)
